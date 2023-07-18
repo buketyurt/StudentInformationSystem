829bebcb5a5280a53512e2b99e696cb808de0ca9
Buket Yurt <buketyurt01@gmail.com>
2023-07-18 19:34:04 +0300

    Initial commit

---
+++ src/main/java/org/example/StudentInformationSystem/Student.java
@@ -0,0 +1,84 @@
+package org.example.StudentInformationSystem;
+
+public class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double matOralNote;
+    double fizikOralNote;
+    double kimyaOralNote;
+    double avarage;
+    boolean isPass;
+
+
+    Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+
+    public void addBulkExamNote(int mat, int fizik, int kimya,double matOralNote,double fizikOralNote,double kimyaOralNote) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+        this.matOralNote = matOralNote;
+        this.fizikOralNote = fizikOralNote;
+        this.kimyaOralNote = kimyaOralNote;
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+
+            double matFinalAverage = mat.calcFinalAverage();
+            double fizikFinalAverage = fizik.calcFinalAverage();
+            double kimyaFinalAverage = kimya.calcFinalAverage();
+            this.avarage = (matFinalAverage + fizikFinalAverage + kimyaFinalAverage) / 3;
+        }
+
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote(){
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+    }
+
+
+}
